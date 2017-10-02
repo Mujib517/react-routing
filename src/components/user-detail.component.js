8e028585d46fb1f5ac39feced4bf2bd90055cf2e
Mujib517 <mujib517@gmail.com>
2017-10-02 14:48:55 +0530

    Nested routing

--- src/components/user-detail.component.js
+++ src/components/user-detail.component.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+import More from './more.component';
+import Reviews from './reviews.component';
 
 export default (props) => {
 
-    return <h1>User Detail {props.match.params.id}</h1>
+    return (
+        <div>
+            <h1>{props.match.params.id}</h1>
+            <div className="nav nav-tabs">
+                <li className={props.location.pathname === '/users/' + props.match.params.id + '/reviews' ? 'active' : 'inactive'}>
+                    <Link to={'/users/' + props.match.params.id + '/reviews'}>Reviews</Link>
+                </li>
+                <li className={props.location.pathname === '/users/' + props.match.params.id + '/more' ? 'active' : 'inactive'}>
+                    <Link to={'/users/' + props.match.params.id + '/more'}>More</Link>
+                </li>
+            </div>
+            <div>
+                <Switch>
+                    <Route path={'/users/' + props.match.params.id + '/more'} exact={true} component={More} />
+                    <Route path={'/users/' + props.match.params.id + '/reviews'} exact={true} component={Reviews} />
+                </Switch>
+            </div>
+
+        </div>
+    )
 }
