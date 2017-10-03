6b28944d80d261af3cd45e7192421319266a4aca
Mujib517 <mujib517@gmail.com>
2017-10-03 12:28:44 +0530

    New User form

--- src/components/new-user.component.js
+++ src/components/new-user.component.js
@@ -4,18 +4,45 @@ class NewUser extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = { user: { isFormValid: false } };
         this.save = this.save.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     save() {
+        console.log(this.state);
         this.props.history.push('/users');
+    }
+
+    onChange(evt) {
+        let name = evt.target.name;
+        let value = evt.target.value;
+
+        let user = this.state.user;
+        user[name] = value;
+        user.isFormValid = this.state.user.name && this.state.user.email ? true : false;;
+
+        this.setState({ user: user });
+
+        console.log(this.state);
     }
 
     render() {
         return (
-            <div>
-                <h1>Add new user</h1>
-                <button onClick={this.save}>Save</button>
+            <div className="col-md-4">
+                <form noValidate="false">
+                    <h1>Add new user</h1>
+                    <div className="form-group">
+                        <input type="text" placeholder="Name" required className="form-control" name="name" onChange={this.onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Email" required className="form-control" name="email" onChange={this.onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input value="Save" type="submit" onClick={this.save} className="btn btn-success" disabled={!this.state.user.isFormValid} />
+                    </div>
+                </form>
             </div>
         )
     }
