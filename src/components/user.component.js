4c207e17b17f6ad38934b19cfc508ce5b22067cb
Rahman <mr185242@ncr.com>
2017-10-06 12:21:53 +0530

    Remove button

--- src/components/user.component.js
+++ src/components/user.component.js
@@ -5,7 +5,15 @@ import { Link } from 'react-router-dom';
 export default (props) => {
     return (
         <div className="well">
-            <Link to={'/users/' + props.user.login} > {props.user.login}</Link>
+            <img src={props.user.avatar_url} width="150" height="150" className="img img-circle" />
+            <div>
+                <Link to={'/users/' + props.user.login} > {props.user.login}</Link>
+                <button className="btn btn-danger btn-sm pull-right">
+                    Remove &nbsp;
+                    <i className="glyphicon glyphicon-remove"></i>
+                </button>
+            </div>
+
         </div>
     );
 }
