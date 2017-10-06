5d1fd3df8480d4d11fdff01b039f505c070ffe49
Rahman <mr185242@ncr.com>
2017-10-06 12:32:45 +0530

    Conver to Class component Install Babel-preset-env

--- src/components/user.component.js
+++ src/components/user.component.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+export default class UserComponent extends React.Component {
 
-export default (props) => {
-    return (
-        <div className="well">
-            <img src={props.user.avatar_url} width="150" height="150" className="img img-circle" />
-            <div>
-                <Link to={'/users/' + props.user.login} > {props.user.login}</Link>
-                <button className="btn btn-danger btn-sm pull-right">
-                    Remove &nbsp;
-                    <i className="glyphicon glyphicon-remove"></i>
-                </button>
+    constructor(props) {
+        super(props);
+    }   
+
+    render() {
+        return (
+            <div className="well">
+                <img src={this.props.user.avatar_url} width="150" height="150" className="img img-circle" />
+                <div>
+                    <Link to={'/users/' + this.props.user.login} > {this.props.user.login}</Link>
+                    <button className="btn btn-danger btn-sm pull-right">
+                        Remove &nbsp;
+                        <i className="glyphicon glyphicon-remove"></i>
+                    </button>
+                </div>
             </div>
-
-        </div>
-    );
+        );
+    }
 }
